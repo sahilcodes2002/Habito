63c30a68bd7e9706837cb6a4b84c4c85fbdebd46
sahilcodes2002 <114796210+sahilcodes2002@users.noreply.github.com>
2024-10-22 04:31:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Join from "./pages/Join";
-import Codeeditor from "./pages/COdeeditor";
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Signup } from './pages/Signup';
+import { Signin } from './pages/Signin';
+import { Dashboard } from './pages/Dashboard';
+import { Settings } from './pages/Settings';
+import { RecoilRoot } from 'recoil';
+import { Homepage } from './pages/Home';
+import { Messages } from './pages/Messages';
+import { Conversation } from './pages/Conversation';
+import { Dashboardnotes } from './pages/Dashboardnotes';
+import { Createtask } from './pages/Createtask';
+import ProjectOptions from './pages/ProjectOptions';
+import { Userprojects } from './pages/Userprojects';
+import { ProjectDetails } from './pages/Projectdetails';
+import { AssignedProjects } from './pages/AssignedProjects';
+import { Notifications } from './pages/Notifications';
+import { Userprofile } from './pages/Userprofile';
+import { Projectmessages } from './pages/Projectmessages';
+import { Workhistory } from './pages/Workhistory';
+import { Colab } from './pages/Colab';
+import { Join } from './pages/Join';
+import Showcode from './pages/Showcode'
+import CodeEditorPage from './pages/Codestore';
+import { Allcodes } from './pages/Allcodes';
 import { Toaster } from "react-hot-toast";
+
 function App() {
   return (
-    <>
-      <div>
+    <div>
+    <div>
         <Toaster
           position="top-right"
           toastOptions={{
@@ -18,13 +39,36 @@ function App() {
           }}
         ></Toaster>
       </div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/join" element={<Join />} />
-          <Route path="/editor/:roomId" element={<Codeeditor />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+      <RecoilRoot>
+        <HashRouter>
+          <Routes>
+            <Route path='/' element={<Homepage />} />
+            {/* <Route path='/signup' element={<Signup />} /> */}
+            <Route path='/signin' element={<Signin />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/dashboardnotes' element={<Dashboardnotes />} />
+            <Route path='/createtask' element={<Createtask />} />
+            <Route path='/settings' element={<Settings />} />
+            <Route path='/messages' element={<Messages />} />
+            <Route path='/projectoptions' element={<ProjectOptions />} />
+            <Route path='/userprojects' element={<Userprojects />} />
+            <Route path='/assignedprojects' element={<AssignedProjects />} />
+            <Route path='/project/:id' element={<ProjectDetails />} />
+            <Route path='/projectmessages/:userId' element={<Projectmessages />} />
+            <Route path='/notifications' element={<Notifications />} />
+            <Route path='/userprofile' element={<Userprofile />} />
+            <Route path='/workhistory' element={<Workhistory />} />
+            <Route path='/colab' element={<Colab />} />
+            <Route path='/signup' element={<Join />} />
+            <Route path='/code' element={<CodeEditorPage />} />
+            <Route path='/showcode/:codeid' element={<Showcode />} />
+            <Route path='/allcode' element={<Allcodes />} />
+            <Route path='/conversations/:userId' element={<Conversation />} />
+            
+          </Routes>
+        </HashRouter>
+      </RecoilRoot>
+    </div>
   );
 }
 
